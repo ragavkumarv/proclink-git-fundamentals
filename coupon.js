12b3273e2a80a1d973d3f176d6b5c2189886328e
ragavkumarv <ragavkumarv@gmail.com>
2024-06-05 15:58:43 +0530

    Splitting function dec & Verified with input for splitting & Reveresed list

--- coupon.js
+++ coupon.js
@@ -1,1 +1,11 @@
 console.log("It works 😊🎊");
+
+function splitting(names) {
+  return names.split(",");
+}
+
+const input = "Bhanu, Sumana, Akhila";
+console.log(splitting(input));
+
+const rev = splitting(input).reverse();
+console.log(rev);
